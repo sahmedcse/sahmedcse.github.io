2a1bff499963ddc82b24cc5fbfd9c74f587ada67
sadatahmed <s78ahmed@uwaterloo.ca>
2016-07-17 00:11:13 -0400

    Added angular js and trying to send email using my own server.

---
+++ js/app.js
@@ -0,0 +1,56 @@
+var app = angular.module("portfolio", []);
+
+app.controller("formController", function($scope, $http) {
+        $scope.emailCheck = true;
+        $scope.subjectCheck = true;
+        $scope.messageCheck = true;
+        $scope.emailError = "";
+
+        $scope.checkForm = function() {
+          if(textChecker($scope.email) || emptyChecker($scope.email)) {
+            $scope.emailError = "This is a required field";
+            $scope.emailCheck = textChecker($scope.email) && emptyChecker($scope.email);
+          } else {
+            $scope.emailError = "Please enter a valid email";
+            $scope.emailCheck = emailChecker($scope.email);
+          }
+          $scope.subjectCheck = !(textChecker($scope.subject) || emptyChecker($scope.subject));
+          $scope.messageCheck = !(textChecker($scope.message) || emptyChecker($scope.message));
+
+          if($scope.emailCheck && $scope.subjectCheck && $scope.messageCheck) {
+            var data = {};
+            data.emailFrom = $scope.email;
+            data.subject = $scope.subject;
+            data.emailContent = $scope.message;
+            data = JSON.stringify(data);
+
+            $scope.sendEmail(data);
+          }
+
+        }
+
+        $scope.sendEmail = function(data) {
+          $http({
+            method : "POST",
+            data : data,
+            url : "https://emailapp.herokuapp.com/email"
+          }).then(function mySucces(response) {
+            console.log("SUCCESS" + response.statusCode);
+          }, function myError(response) {
+            console.log("ERROR " + response.statusCode);
+          });
+        }
+});
+
+function emailChecker(emailValue) {
+  var expr = new RegExp("[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,3}");
+  return expr.test(emailValue);
+}
+
+function textChecker(textValue) {
+  return textValue == null;
+}
+
+function emptyChecker(textValue) {
+  return textValue === "";
+}
